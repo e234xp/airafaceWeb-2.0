cbaf34ade5b4e0e6c0369ee1b31d668a8c705069
boll840113 <stephy.tsai@aira.com.tw>
2023-07-03 18:40:35 +0800

    I/Obox版面完成 i18n定義Wiegand 欄位名稱

--- src/views/outputdevice/forms/IOboxesBasic.vue
+++ src/views/outputdevice/forms/IOboxesBasic.vue
@@ -16,108 +16,142 @@
             <h2 sm="12">{{ disp_header }}</h2>  
           </div>
           <!-- Basic -->
-          <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_basicDeviceName }}</CRow>
-          <CRow>
-            <CCol sm="6">
-              <CInput size="lg"  class="h5"  style="width: 100%;" />
-            </CCol>
-          </CRow>
-
-          <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_basicDeviceGroups }}</CRow>
-          <CRow>
-            <CCol sm="6">
-              <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control">
-              </v-select>
-            </CCol>
-          </CRow>
-
-          <div style="height: 35px"></div>
-
-          <!-- Video Source -->
-          <div>
-            <h2 sm="12">{{ disp_subtitle }}</h2>
-          </div>
-          
-          <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_Type }}</CRow>
-          <CRow>
-            <CCol sm="6">
-              <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control">
-              </v-select>
-            </CCol>
-          </CRow>
-
-          <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_ConnectionString }}</CRow>
-          <CRow>
-            <CCol sm="12">
-              <CInput size="lg"  class="h5"  style="width: 100%;" />
-            </CCol>
-          </CRow>
-
-          <!-- ROI -->
-          <!-- <CTab>
-            <template slot="title">
-              <CIcon name="cil-crop" /> ROI
-            </template>
-            <div class="py-2"></div>
-            <CButton color="primary" @click="getImage"> 取得影像
-            </CButton>
-            &nbsp;
-            <CButton color="primary" @click="removeRegion"> {{ $t('Devices.Remove_ROI') }}
-            </CButton>
-
-            <div style="display: flex; justify-content: center; align-items: center;">
-              <div class="form-row">
-                <div class="form-group col-md-12"
-                  style="display: flex; justify-content: center; align-items: center;">
-                  <canvas ref="sketchpad" id="canvas" width="1024" height="576"></canvas>
-                </div>
-              </div>
-            </div>
-
-          </CTab> -->
-
-          <!-- Face Capture -->
           <div class="mt-3">
             <CRow sm="12">
-              <CCol sm="6">
-                標題1: Face minimum width:
-                <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <CCol sm="6" class="h5" >
+                {{ disp_IOBoxesBasicBrand }}
+                <v-select  v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control mt-2">
+                </v-select>
               </CCol>
-              <CCol sm="6">
-                標題2: Face minimum height:
-                <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicModel }}
+                <v-select  v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control mt-2">
+                </v-select>
               </CCol>
             </CRow>
           </div>
 
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" v-model="isChecked" value="" id="checkbox1">
+          <div class="mt-3">
+            <CRow sm="12">
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicDeviceName }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+              <CCol sm="6" class="h5" >
+                {{ disp_IOBoxesBasicDeviceGroups }}
+                <v-select  v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control mt-2">
+                </v-select>
+              </CCol>
+            </CRow>
+          </div>
+
+          <div style="height: 35px"></div>
+
+          <!-- Connect -->
+          <div>
+            <h2 sm="12">{{ disp_ConnectionTitle }}</h2>
+          </div>
+          
+          <div class="mt-3">
+            <CRow sm="12">
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicIP }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicPort }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+            </CRow>
+          </div>
+
+          <div class="mt-3">
+            <CRow sm="12">
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicUserName }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicPassword }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+            </CRow>
+          </div>
+
+          <div style="height: 35px"></div>
+
+          <!-- Digital OutPut1 -->
+          <div>
+            <h2 sm="12">{{ disp_DigitalOutPut1Title }}</h2>
+          </div>
+
+          <div class="form-check mb-3 ml-2">
+            <input class="form-check-input" type="checkbox" v-model="isChecked" value="" id="flexCheckDefault">
             <label class="form-check-label mt-2" for="flexCheckDefault">
-              Frontal face filter
+              {{ disp_IOBoxesBasicEnable }}
             </label>
           </div>
-          <!-- <CInputCheckbox class="form-check ml-1 mb-4" v-model="isChecked" id="checkbox1" label="Frontal face filter" /> -->
-
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Target score(0~1):</CRow>
+          
+          <CRow sm="6" class="h5 ml-2 mb-3">{{ disp_IOBoxesBasicDefaultValue }}</CRow>
           <CRow>
             <CCol sm="6">
-              <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control">
+              </v-select>
             </CCol>
           </CRow>
 
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Capture interval:</CRow>
+          <div class="mt-3">
+            <CRow sm="12">
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicValueWhenTriggered }}
+                <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control mt-2">
+                </v-select>
+              </CCol>
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicDurationWhenTriggered }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+            </CRow>
+          </div>
+          
+          <div style="height: 35px"></div>
+
+          <!-- Digital OutPut2 -->
+
+          <div>
+            <h2 sm="12">{{  disp_DigitalOutPut2Title }}</h2>
+          </div>
+
+          <div class="form-check mb-3 ml-2">
+            <input class="form-check-input" type="checkbox" v-model="isChecked" value="" id="flexCheckDefault">
+            <label class="form-check-label mt-2" for="flexCheckDefault">
+              {{ disp_IOBoxesBasicEnable }}
+            </label>
+          </div>
+          
+          <CRow sm="6" class="h5 ml-2 mb-3">{{ disp_IOBoxesBasicDefaultValue }}</CRow>
           <CRow>
             <CCol sm="6">
-              <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control">
+              </v-select>
             </CCol>
           </CRow>
 
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Anti-spoofing score(0~1)</CRow>
-          <CRow>
-            <CCol sm="6">
-              <CInput size="lg"  class="h5"  style="width: 100%;" />
-            </CCol>
-          </CRow>
+          <div class="mt-3">
+            <CRow sm="12">
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicValueWhenTriggered }}
+                <v-select v-model="value_deviceGroups" :options="value_deviceGroupsList"  :filterable="true" class="font-control mt-2">
+                </v-select>
+              </CCol>
+              <CCol sm="6" class="h5"  >
+                {{ disp_IOBoxesBasicDurationWhenTriggered }}
+                <CInput size="xl" class="mt-2" style="width: 100%;" />
+              </CCol>
+            </CRow>
+          </div>
+          
+          <div style="height: 35px"></div>
 
 
           <!-- 儲存按鈕 -->
@@ -158,25 +192,36 @@
         isChecked: true,
 
         /*Basic title  */
-        disp_header: i18n.formatter.format("VideoDeviceBasic"),
+        disp_header: i18n.formatter.format("I/OBoxesBasicName"),
 
         /**content */
-        disp_basicDeviceName: i18n.formatter.format("BasicDeviceName"),
-        disp_basicDeviceGroups: i18n.formatter.format("BasicDeviceGroups"),
+        disp_IOBoxesBasicBrand: i18n.formatter.format("I/OBoxesBasicCOlNameBrand"),
+        disp_IOBoxesBasicModel: i18n.formatter.format("I/OBoxesBasicCOlNameModel"),
+        disp_IOBoxesBasicDeviceName: i18n.formatter.format("I/OBoxesBasicCOlNameDeviceName"),
+        disp_IOBoxesBasicDeviceGroups: i18n.formatter.format("I/OBoxesBasicCOlNameDeviceGroups"),
 
-        /*Video Source title  */
-        disp_subtitle: i18n.formatter.format("VideoDeviceVideoSource"),
-
-        /**content */
-        disp_Type: i18n.formatter.format("VideoSourceType"),
-        disp_ConnectionString: i18n.formatter.format("VideoSourceConnectionString"),
-
-        /*Face Capture title  */
-        // disp_header: i18n.formatter.format("VideoDeviceBasic"),
+        /*Connection title  */
+        disp_ConnectionTitle: i18n.formatter.format("I/OBoxesBasicTitleNameConnection"),
 
         /**content */
-        // disp_basicDeviceName: i18n.formatter.format("BasicDeviceName"),
-        // disp_basicDeviceGroups: i18n.formatter.format("BasicDeviceGroups"),
+        disp_IOBoxesBasicIP: i18n.formatter.format("I/OBoxesBasicCOlNameIP"),
+        disp_IOBoxesBasicPort: i18n.formatter.format("I/OBoxesBasicCOlNamePort"),
+        disp_IOBoxesBasicUserName: i18n.formatter.format("I/OBoxesBasicCOlNameUserName"),
+        disp_IOBoxesBasicPassword: i18n.formatter.format("I/OBoxesBasicCOlNamePassword"),
+
+        // /*Digital output1 title  */
+        disp_DigitalOutPut1Title: i18n.formatter.format("I/OBoxesBasicTitleNameDigitalOutPut1"),
+
+        // /**content */
+        disp_IOBoxesBasicEnable: i18n.formatter.format("I/OBoxesBasicCOlNameEnable"),
+        disp_IOBoxesBasicDefaultValue: i18n.formatter.format("I/OBoxesBasicCOlNameDefaultValue"),
+        disp_IOBoxesBasicValueWhenTriggered: i18n.formatter.format("I/OBoxesBasicCOlNameValueWhenTriggered"),
+        disp_IOBoxesBasicDurationWhenTriggered: i18n.formatter.format("I/OBoxesBasicCOlNameDurationWhenTriggered"),
+
+        // /*Digital output2 title  */
+        disp_DigitalOutPut2Title: i18n.formatter.format("I/OBoxesBasicTitleNameDigitalOutPut2"),
+
+
 
 
         disp_save: i18n.formatter.format("Save"),
