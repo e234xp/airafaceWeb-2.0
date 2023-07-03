4ac2313294b643c714518bf4728ce3d809657d55
boll840113 <stephy.tsai@aira.com.tw>
2023-07-03 16:55:34 +0800

    將CamerasBasic文字替完成i18n翻譯文字 並調整css及補上小標題

--- src/views/videodevice/forms/CamerasBasic.vue
+++ src/views/videodevice/forms/CamerasBasic.vue
@@ -77,15 +77,18 @@
           </CTab> -->
 
           <!-- Face Capture -->
+          <div>
+            <h2 sm="12">{{ disp_subtitleFaceCapture }}</h2>
+          </div>
           <div class="mt-3">
             <CRow sm="12">
-              <CCol sm="6">
-                標題1: Face minimum width:
-                <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <CCol sm="6" class="h5" >
+                {{ disp_faceMinimumWidth }}
+                <CInput size="lg" class="mt-3" style="width: 100%;" />
               </CCol>
-              <CCol sm="6">
-                標題2: Face minimum height:
-                <CInput size="lg"  class="h5"  style="width: 100%;" />
+              <CCol sm="6" class="h5"  >
+                {{ disp_faceMinimumHeight }}
+                <CInput size="lg" class="mt-3" style="width: 100%;" />
               </CCol>
             </CRow>
           </div>
@@ -93,26 +96,26 @@
           <div class="form-check">
             <input class="form-check-input" type="checkbox" v-model="isChecked" value="" id="checkbox1">
             <label class="form-check-label mt-2" for="flexCheckDefault">
-              Frontal face filter
+              {{ disp_frontalFaceFilter }}
             </label>
           </div>
           <!-- <CInputCheckbox class="form-check ml-1 mb-4" v-model="isChecked" id="checkbox1" label="Frontal face filter" /> -->
 
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Target score(0~1):</CRow>
+          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_targetScore }}</CRow>
           <CRow>
             <CCol sm="6">
               <CInput size="lg"  class="h5"  style="width: 100%;" />
             </CCol>
           </CRow>
 
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Capture interval:</CRow>
+          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_captureInterval }}</CRow>
           <CRow>
             <CCol sm="6">
               <CInput size="lg"  class="h5"  style="width: 100%;" />
             </CCol>
           </CRow>
 
-          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">Anti-spoofing score(0~1)</CRow>
+          <CRow sm="6" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_antiSpoofingScore }}</CRow>
           <CRow>
             <CCol sm="6">
               <CInput size="lg"  class="h5"  style="width: 100%;" />
@@ -172,12 +175,16 @@
         disp_ConnectionString: i18n.formatter.format("VideoSourceConnectionString"),
 
         /*Face Capture title  */
-        // disp_header: i18n.formatter.format("VideoDeviceBasic"),
+        disp_subtitleFaceCapture: i18n.formatter.format("VideoFaceCapture"),
 
         /**content */
-        // disp_basicDeviceName: i18n.formatter.format("BasicDeviceName"),
-        // disp_basicDeviceGroups: i18n.formatter.format("BasicDeviceGroups"),
+        disp_faceMinimumWidth: i18n.formatter.format("VideoBasicCOlNameFaceMinimumWidth"),
+        disp_faceMinimumHeight: i18n.formatter.format("VideoBasicCOlNameFaceMinimumHeight"),
+        disp_frontalFaceFilter: i18n.formatter.format("VideoBasicCOlNameFrontalFaceFilter"),
 
+        disp_targetScore: i18n.formatter.format("VideoBasicCOlNameTargetScore"),
+        disp_captureInterval: i18n.formatter.format("VideoBasicCOlNameCaptureInterval"),
+        disp_antiSpoofingScore: i18n.formatter.format("VideoBasicCOlNameAntiSpoofingScore"),
 
         disp_save: i18n.formatter.format("Save"),
 
@@ -187,7 +194,7 @@
       };
     },
     components: {
-    "v-select": VueSelect,
+      "v-select": VueSelect,
     },
     computed: {
       ...mapState(["ellipsisMode"]),
