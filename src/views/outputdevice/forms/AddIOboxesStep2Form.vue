e521c12f85084f2b973319118a4ab6078cdf4177
boll840113 <stephy.tsai@aira.com.tw>
2023-07-24 16:47:21 +0800

    將步驟二頁面資料定義乾淨，方法先寫好

--- src/views/outputdevice/forms/AddIOboxesStep2Form.vue
+++ src/views/outputdevice/forms/AddIOboxesStep2Form.vue
@@ -6,16 +6,16 @@
       </div>
     
       <!-- 項目 -->
-      <!-- Connect -->
+      <!-- Connection -->
       <div class="mt-3">
           <CRow sm="12">
           <CCol sm="6" class="h5"  >
               {{ disp_IOBoxesBasicIP }}
-              <CInput size="lg" class="mt-2" style="width: 100%;" />
+              <CInput size="lg" class="mt-2" v-model="localStep2form.ip_address" />
           </CCol>
           <CCol sm="6" class="h5"  >
               {{ disp_IOBoxesBasicPort }}
-              <CInput size="lg" class="mt-2" style="width: 100%;" />
+              <CInput size="lg" class="mt-2" v-model="localStep2form.port" />
           </CCol>
           </CRow>
       </div>
@@ -24,11 +24,11 @@
           <CRow sm="12">
           <CCol sm="6" class="h5"  >
               {{ disp_IOBoxesBasicUserName }}
-              <CInput size="lg" class="mt-2" style="width: 100%;" />
+              <CInput size="lg" class="mt-2" v-model="localStep2form.user" />
           </CCol>
           <CCol sm="6" class="h5"  >
               {{ disp_IOBoxesBasicPassword }}
-              <CInput size="lg" class="mt-2" style="width: 100%;" type="password"/>
+              <CInput size="lg" class="mt-2" v-model="localStep2form.pass" type="password"/>
           </CCol>
           </CRow>
       </div>
@@ -38,39 +38,22 @@
   </template>
     
   <script>
-    import { mapState } from "vuex";
-    import TableObserver from "@/utils/TableObserver.vue";
     import i18n from "@/i18n";
 
     import VueSelect from 'vue-select';
     import Multiselect from "vue-multiselect";
     import "@/airacss/vue-multiselect.css";
-      
-
-
 
     export default {
-      name: "CamerasBasic",
+      name: "AddCamerasStep1Form",
+      props:{
+        step2form: Object
+      },
       data() {
-        return {
-          value_dataItemsToShow: [{enable:false,name:'',timestamp:'',remark:'',modifier:'',remark1:''}],
-          value_allTableItems: [],
-          value_tablePage: {
-            currentPage: 1,
-            pageSize: 5,
-            totalResult: 0,
-          },
-          value_searchingFilter: "",
+      return {
+          localStep2form: { ...this.step2form },
+
           isChecked: true,
-
-          /*Basic title  */
-          disp_header: i18n.formatter.format("I/OBoxesBasicName"),
-
-          /**content */
-          disp_IOBoxesBasicBrand: i18n.formatter.format("I/OBoxesBasicCOlNameBrand"),
-          disp_IOBoxesBasicModel: i18n.formatter.format("I/OBoxesBasicCOlNameModel"),
-          disp_IOBoxesBasicDeviceName: i18n.formatter.format("I/OBoxesBasicCOlNameDeviceName"),
-          disp_IOBoxesBasicDeviceGroups: i18n.formatter.format("I/OBoxesBasicCOlNameDeviceGroups"),
 
           /*Connection title  */
           disp_ConnectionTitle: i18n.formatter.format("I/OBoxesBasicTitleNameConnection"),
@@ -81,23 +64,6 @@
           disp_IOBoxesBasicUserName: i18n.formatter.format("I/OBoxesBasicCOlNameUserName"),
           disp_IOBoxesBasicPassword: i18n.formatter.format("I/OBoxesBasicCOlNamePassword"),
 
-          // /*Digital output1 title  */
-          disp_DigitalOutPut1Title: i18n.formatter.format("I/OBoxesBasicTitleNameDigitalOutPut1"),
-
-          // /**content */
-          disp_IOBoxesBasicEnable: i18n.formatter.format("I/OBoxesBasicCOlNameEnable"),
-          disp_IOBoxesBasicDefaultValue: i18n.formatter.format("I/OBoxesBasicCOlNameDefaultValue"),
-          disp_IOBoxesBasicValueWhenTriggered: i18n.formatter.format("I/OBoxesBasicCOlNameValueWhenTriggered"),
-          disp_IOBoxesBasicDurationWhenTriggered: i18n.formatter.format("I/OBoxesBasicCOlNameDurationWhenTriggered"),
-
-          // /*Digital output2 title  */
-          disp_DigitalOutPut2Title: i18n.formatter.format("I/OBoxesBasicTitleNameDigitalOutPut2"),
-
-
-
-
-          disp_save: i18n.formatter.format("Save"),
-
           /**v-model */
           value_deviceGroups: "", /**選單 */
           value_deviceGroupsList: [1,2,3]
@@ -105,14 +71,33 @@
       },
       components: {
         "v-select": VueSelect,
-        Multiselect: Multiselect
+        multiselect: Multiselect,
       },
-      computed: {
-        ...mapState(["ellipsisMode"]),
+      //預設值
+      created() {
+        this.defaultPortValue();
+        this.localStep2form.user = "admin",
+        this.localStep2form.pass = "123456"
+      }, 
+      // 拿資料 寫入資料
+      watch: {
+        localStep2form: {
+          handler(newValue) {
+            console.log('emit updateStep2form')
+            this.$emit('updateStep2form', { ...newValue });
+          },
+          deep: true,
+        },
       },
-      
       methods: {
-
+        defaultPortValue() {
+          this.localStep2form.port = 554;
+          return this.localStep2form.port !== null && this.localStep2form.port >= 0 && this.localStep2form.port <= 65535;
+        },
+        // 判斷欄位空值
+        isNotEmpty(value) {
+          return value !== null && value !== undefined && value !== '';
+        }
       },
     }
   </script>
