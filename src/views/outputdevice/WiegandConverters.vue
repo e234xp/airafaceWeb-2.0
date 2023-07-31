2be0b5b98e51e2c25a5594bbb70e9139401d48d4
boll840113 <stephy.tsai@aira.com.tw>
2023-07-31 16:12:33 +0800

    將韋根架構分好，並修改搜尋清單頁面

--- src/views/outputdevice/WiegandConverters.vue
+++ src/views/outputdevice/WiegandConverters.vue
@@ -2,142 +2,110 @@
   <div>
     <CRow>
       <CCol sm="12">
-        <WiegandConvertersForm :formData="$data" :onAdd="onAdd" :onDelete="onDelete" :onModify="onModify" :onFetchDataCallback="onFetchDataCallback"/>
+        <ListForm
+          :onAdd="onAdd"
+          :onDelete="onDelete"
+          :onModify="onModify"
+          :onGetItems="onGetItems"
+        />
       </CCol>
     </CRow>
   </div>
 </template>
-  
+
 <script>
-  import WiegandConvertersForm from './forms/WiegandConvertersForm.vue'
+import ListForm from "@/modules/outputdevice/wiegandlist/ListForm.vue";
 
-  import { mapState } from "vuex";
-  import TableObserver from "@/utils/TableObserver.vue";
-  import i18n from "@/i18n";
+import TableObserver from "@/utils/TableObserver.vue";
+import i18n from "@/i18n";
 
-  export default {
-    name: "Cameras",
-    mixins: [TableObserver],
-    components: { WiegandConvertersForm },
-    data() {
-      return {
-        value_dataItemsToShow: [{enable:false,name:'',timestamp:'',remark:'',modifier:'',remark1:''}],
-        value_allTableItems: [],
-        value_tablePage: {
-          currentPage: 1,
-          pageSize: 5,
-          totalResult: 0,
+export default {
+  name: "CameraList",
+  mixins: [TableObserver],
+  components: { ListForm },
+  methods: {
+    async onGetItems() {
+      return this.getItems(0, 3000);
+    },
+
+    async getItems(shift, sliceSize) {
+      const ret = await this.$globalFindWiegandConverters("", shift, sliceSize);
+      const {
+        data: { list: dataList },
+        error,
+      } = ret;
+
+      if (error) {
+        this.$fire({
+          title: i18n.formatter.format("NetworkLoss"),
+          text: "",
+          type: "error",
+          timer: 3000,
+          confirmButtonColor: "#20a8d8",
+        });
+      }
+
+      return dataList;
+    },
+
+    // 新增
+    onAdd() {
+      this.$router.push({
+        name: "AddWiegandConverters",
+        params: {
+          value_returnRoutePath: "WiegandConverters",
+          value_returnRouteName: i18n.formatter.format("Return"),
         },
-        value_searchingFilter: "",
-
-        disp_header: i18n.formatter.format("VideoDeviceCameras"),
-        disp_search: i18n.formatter.format("Search"),
-        disp_add: i18n.formatter.format("Add"),
-        disp_delete: i18n.formatter.format("Delete"),
-
-
-        /*data table */
-        disp_enable: i18n.formatter.format("Enable"),
-        disp_deviceName: i18n.formatter.format("DeviceName"),
-        disp_status: i18n.formatter.format("DeviceStatus"),
-        disp_ipAddress: i18n.formatter.format("IpAddress"),
-        disp_In: i18n.formatter.format("EnableIn"),
-        disp_out: i18n.formatter.format("EnableOut"),
-
-      };
+      });
     },
-    computed: {
-      ...mapState(["ellipsisMode"]),
-    },
-    methods: {
-      // 新增
-      onAdd() {
-        this.$router.push({
-          name: 'AddWiegandConverters', params: {
-            value_returnRoutePath: "WiegandConverters",
-            value_returnRouteName: i18n.formatter.format("Return")
-          }
-        });
-      },
-      onFetchDataCallback(cb) {
-        const self = this;
-        self.flag_keepingDownload = true;
-        self.downloadTableItemsAsync(0,3000, cb);
-      },
-      
-      async downloadTableItemsAsync(shitf,sliceSize, cb) {
-        const self = this;
-        let reset = true;
-        let thereIsMoreData = true;
-        let ret = await self.$globalFindCameras("", shitf, sliceSize, cb);
-        //console.log(ret,"拿到的資料")
-        const list = ret.data.camera_list;
-        //console.log(list,"拿到的資料list")
-        const error = ret.error;
-        if( error == null ) {
-          if(cb) cb( null, reset, thereIsMoreData, list );
-          //console.log(null, true, false, list,"QQQ")
 
-        }
-        else {
-          if( cb ) cb( error, true, false, [] );
-          self.$fire({
-            title: i18n.formatter.format("NetworkLoss"),
-            text: "",
-            type: "error",
-            timer: 3000,
-            confirmButtonColor: "#20a8d8"
-          });
-        }
-      },
-      // 刪除
-      onDelete(items, cb) {
-        const self = this;
-        if (items && Array.isArray(items)) {
-          let uuidListToDel = [];
-          items.forEach(item => {
-            uuidListToDel.push(item.uuid);
-          });
-          self.$confirm("", i18n.formatter.format("ConfirmToDelete"), "question", {
-            confirmButtonText: i18n.formatter.format("Confirm"),
-            cancelButtonText: i18n.formatter.format("Cancel"),
-            confirmButtonColor: "#20a8d8",
-            cancelButtonColor: "#f86c6b"
-          }).then((v) => {
-            self.RemoveCameraAsync(uuidListToDel, cb);
-          }).catch((e) => {
-            if (cb) cb(false);
-          });
-        }
-      },
-      //修改
-      async onModify(item) {
-        this.$router.push({
-          name: 'ModifyWiegandConverters', params: {
-            value_returnRoutePath: "WiegandConverters",
-            value_returnRouteName: i18n.formatter.format("Return"),
-            item: item,
-          }
-        });
-      },
-      async RemoveCameraAsync(uuid, cb) {
-        const self = this;
-        let ret = await self.$globalRemoveCameras(uuid);
-        const error = ret.error;
-        if (error) {
+    //修改
+    async onModify(item) {
+      this.$router.push({
+        name: "ModifyWiegandConverters",
+        params: {
+          value_returnRoutePath: "WiegandConverters",
+          value_returnRouteName: i18n.formatter.format("Return"),
+          item: item,
+        },
+      });
+    },
+
+    // 刪除
+    onDelete(items, cb) {
+      if (!items || !Array.isArray(items)) return;
+
+      const uuidListToDel = items.map(({ uuid }) => uuid);
+      this.$confirm("", i18n.formatter.format("ConfirmToDelete"), "question", {
+        confirmButtonText: i18n.formatter.format("Confirm"),
+        cancelButtonText: i18n.formatter.format("Cancel"),
+        confirmButtonColor: "#20a8d8",
+        cancelButtonColor: "#f86c6b",
+      })
+        .then(() => {
+          this.deleteWiegand(uuidListToDel, cb);
+        })
+        .catch(() => {
           if (cb) cb(false);
-          self.$fire({
-            text: i18n.formatter.format("OperationFailed"),
-            type: "error",
-            timer: 3000,
-            confirmButtonColor: "#20a8d8",
-            confirmButtonText: i18n.formatter.format("OK")
-          });
-        }
-        else if (cb) cb(true);
-      },
-     
+        });
     },
-  }
+
+    async deleteWiegand(uuid, cb) {
+      const ret = await this.$globalRemoveWiegandConverters(uuid);
+      const { error } = ret;
+
+      if (error) {
+        if (cb) cb(false);
+        this.$fire({
+          text: i18n.formatter.format("OperationFailed"),
+          type: "error",
+          timer: 3000,
+          confirmButtonColor: "#20a8d8",
+          confirmButtonText: i18n.formatter.format("OK"),
+        });
+      }
+      if (cb) cb(true);
+    },
+  },
+};
 </script>
-  
