f5bdc1e3a59883cde90f236eec0207c5ece184b1
boll840113 <stephy.tsai@aira.com.tw>
2023-08-02 11:32:05 +0800

    OutputDeviceGroups 建置搜尋頁面func

--- src/views/outputdevice/OutputDeviceGroups.vue
+++ src/views/outputdevice/OutputDeviceGroups.vue
@@ -2,139 +2,110 @@
   <div>
     <CRow>
       <CCol sm="12">
-        <ListForm :formData="$data" :onAdd="onAdd" :onDelete="onDelete" :onModify="onModify" :onFetchDataCallback="onFetchDataCallback"/>
+        <ListForm
+          :onAdd="onAdd"
+          :onDelete="onDelete"
+          :onModify="onModify"
+          :onGetItems="onGetItems"
+        />
       </CCol>
     </CRow>
   </div>
 </template>
-  
+
 <script>
-  import ListForm from "@/modules/outputdevice/outputdevicegroupslist/ListForm.vue";
+import ListForm from "@/modules/outputdevice/outputdevicegroupslist/ListForm.vue";
 
-  import { mapState } from "vuex";
-  import TableObserver from "@/utils/TableObserver.vue";
-  import i18n from "@/i18n";
+import TableObserver from "@/utils/TableObserver.vue";
+import i18n from "@/i18n";
 
-  export default {
-    name: "OutputDeviceGroups",
-    mixins: [TableObserver],
-    components: { ListForm },
-    data() {
-      return {
-        value_dataItemsToShow: [],
-        value_allTableItems: [],
-        value_tablePage: {
-          currentPage: 1,
-          pageSize: 5,
-          totalResult: 0,
+export default {
+  name: "OutputDeviceGroups",
+  mixins: [TableObserver],
+  components: { ListForm },
+  methods: {
+    async onGetItems() {
+      return this.getItems(0, 3000);
+    },
+
+    async getItems(shift, sliceSize) {
+      const ret = await this.$globalFindOutputDeviceGroups("", shift, sliceSize);
+      const {
+        data: { result: dataList },
+        error,
+      } = ret;
+
+      if (error) {
+        this.$fire({
+          title: i18n.formatter.format("NetworkLoss"),
+          text: "",
+          type: "error",
+          timer: 3000,
+          confirmButtonColor: "#20a8d8",
+        });
+      }
+
+      return dataList;
+    },
+
+    // 新增
+    onAdd() {
+      this.$router.push({
+        name: "AddOutputDeviceGroups",
+        params: {
+          value_returnRoutePath: "OutputDeviceGroups",
+          value_returnRouteName: i18n.formatter.format("Return"),
         },
-        value_searchingFilter: "",
-
-        disp_header: i18n.formatter.format("DevicesGroupTitle"),
-        disp_search: i18n.formatter.format("Search"),
-        disp_add: i18n.formatter.format("Add"),
-        disp_delete: i18n.formatter.format("Delete"),
-
-
-        /*data table */
-        disp_group: i18n.formatter.format("VideoDevicesGroup"),
-        disp_videoDevices: i18n.formatter.format("VideoDevices"),
-        disp_outputDevices: i18n.formatter.format("OutputDevices"),
-        disp_rules: i18n.formatter.format("Rules"),
-
-      };
+      });
     },
-    computed: {
-      ...mapState(["ellipsisMode"]),
-    },
-    methods: {
-      // 新增
-      onAdd() {
-        this.$router.push({
-          name: 'AddOutputDeviceGroups', params: {
-            value_returnRoutePath: "OutputDeviceGroups",
-            value_returnRouteName: i18n.formatter.format("Return")
-          }
-        });
-      },
-      onFetchDataCallback(cb) {
-        const self = this;
-        self.flag_keepingDownload = true;
-        self.downloadTableItemsAsync(0,3000, cb);
-      },
-      
-      async downloadTableItemsAsync(shitf,sliceSize, cb) {
-        const self = this;
-        let reset = true;
-        let thereIsMoreData = true;
-        let ret = await self.$globalFindCameras("", shitf, sliceSize, cb);
-        //console.log(ret,"拿到的資料")
-        const list = ret.data.camera_list;
-        //console.log(list,"拿到的資料list")
-        const error = ret.error;
-        if( error == null ) {
-          if(cb) cb( null, reset, thereIsMoreData, list );
-          //console.log(null, true, false, list,"QQQ")
 
-        }
-        else {
-          if( cb ) cb( error, true, false, [] );
-          self.$fire({
-            title: i18n.formatter.format("NetworkLoss"),
-            text: "",
-            type: "error",
-            timer: 3000,
-            confirmButtonColor: "#20a8d8"
-          });
-        }
-      },
-      // 刪除
-      onDelete(items, cb) {
-        const self = this;
-        if (items && Array.isArray(items)) {
-          let uuidListToDel = [];
-          items.forEach(item => {
-            uuidListToDel.push(item.uuid);
-          });
-          self.$confirm("", i18n.formatter.format("ConfirmToDelete"), "question", {
-            confirmButtonText: i18n.formatter.format("Confirm"),
-            cancelButtonText: i18n.formatter.format("Cancel"),
-            confirmButtonColor: "#20a8d8",
-            cancelButtonColor: "#f86c6b"
-          }).then((v) => {
-            self.RemoveCameraAsync(uuidListToDel, cb);
-          }).catch((e) => {
-            if (cb) cb(false);
-          });
-        }
-      },
-      //修改
-      async onModify(item) {
-        this.$router.push({
-          name: 'ModifyWiegandConverters', params: {
-            value_returnRoutePath: "WiegandConverters",
-            value_returnRouteName: i18n.formatter.format("Return"),
-            item: item,
-          }
-        });
-      },
-      async RemoveCameraAsync(uuid, cb) {
-        const self = this;
-        let ret = await self.$globalRemoveCameras(uuid);
-        const error = ret.error;
-        if (error) {
+    //修改
+    async onModify(item) {
+      this.$router.push({
+        name: "ModifyOutputDeviceGroups",
+        params: {
+          value_returnRoutePath: "OutputDeviceGroups",
+          value_returnRouteName: i18n.formatter.format("Return"),
+          item: item,
+        },
+      });
+    },
+
+    // 刪除
+    onDelete(items, cb) {
+      if (!items || !Array.isArray(items)) return;
+
+      const uuidListToDel = items.map(({ uuid }) => uuid);
+      this.$confirm("", i18n.formatter.format("ConfirmToDelete"), "question", {
+        confirmButtonText: i18n.formatter.format("Confirm"),
+        cancelButtonText: i18n.formatter.format("Cancel"),
+        confirmButtonColor: "#20a8d8",
+        cancelButtonColor: "#f86c6b",
+      })
+        .then(() => {
+          this.deleteOutputDeviceGroups(uuidListToDel, cb);
+        })
+        .catch(() => {
           if (cb) cb(false);
-          self.$fire({
-            text: i18n.formatter.format("OperationFailed"),
-            type: "error",
-            timer: 3000,
-            confirmButtonColor: "#20a8d8",
-            confirmButtonText: i18n.formatter.format("OK")
-          });
-        }
-        else if (cb) cb(true);
-      },  
+        });
     },
-  }
+
+    async deleteOutputDeviceGroups(uuid, cb) {
+      const ret = await this.$globalRemoveOutputDeviceGroup(uuid);
+      const { error } = ret;
+
+      if (error) {
+        if (cb) cb(false);
+        this.$fire({
+          text: i18n.formatter.format("OperationFailed"),
+          type: "error",
+          timer: 3000,
+          confirmButtonColor: "#20a8d8",
+          confirmButtonText: i18n.formatter.format("OK"),
+        });
+      }
+      if (cb) cb(true);
+    },
+  },
+};
 </script>
-  
