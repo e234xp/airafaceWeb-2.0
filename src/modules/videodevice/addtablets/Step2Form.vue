9171a2698a6f90ffde3e84e273ee57ed8797c0d3
boll840113 <stephy.tsai@aira.com.tw>
2023-08-09 17:19:03 +0800

    步驟二，基本設定版面完成

--- src/modules/videodevice/addtablets/Step2Form.vue
+++ src/modules/videodevice/addtablets/Step2Form.vue
@@ -1,195 +1,106 @@
-  <template>
-    <div id="wrapper">
-      <!-- 搜尋欄跟按鈕 -->
-      <div>
-        <CCol sm="12">
-          <CRow>
-            <div style="margin-left: auto">
-              <CInput v-model.lazy="value_searchingFilter"  style="width: 400px" size="lg" :placeholder="disp_search" >
-                <template #prepend-content>
-                  <CIcon name="cil-search" />
-                </template>
-              </CInput>
-            </div>
-          </CRow>
+<template>
+  <div id="wrapper">
+    <!-- 標題 -->
+    <div>
+      <h2 sm="12">{{ disp_faceAccessTitle }}</h2>
+    </div>
+
+    <!-- 項目 -->
+    <!-- Face access -->
+    <div class="mt-3">
+
+      <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_recognitionThreshold }}</CRow>
+      <CRow>
+        <CCol sm="6">
+          <CInput size="lg"  class="h5"  style="width: 100%;" />
         </CCol>
-      </div>
-        <!-- 表格 -->
-      <CCard>
-        <CCardBody>
-          <!-- {{ value_dataItemsToShow }} -->
-          <div>
-            <vxe-table :data="value_dataItemsToShow" stripe align="center" :cell-style="cellStyle"
-              :header-cell-style="headerCellStyle" ref="mainTable" :auto-resize="true" keep-source
-                highlight-current-row :edit-config="{ trigger: 'manual', mode: 'row' }">
+      </CRow>
 
-              <vxe-table-column type="checkbox" align="center" width="auto"></vxe-table-column>
+      <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_faceCaptureInternal }}</CRow>
+      <CRow>
+        <CCol sm="6">
+          <CInput size="lg"  class="h5"  style="width: 100%;" />
+        </CCol>
+      </CRow>
 
-              <vxe-table-column :show-overflow="ellipsisMode" field="name" :title="disp_outputDevices" width="30%" align="center">
-              </vxe-table-column>
+      <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_faceOverlapRatio }}</CRow>
+      <CRow>
+        <CCol sm="6">
+          <CInput size="lg"  class="h5"  style="width: 100%;" />
+        </CCol>
+      </CRow>
 
-              <vxe-table-column :show-overflow="ellipsisMode" field="group" :title="disp_group" align="center" width="auto"></vxe-table-column>
+      <CRow sm="12" class="h5 ml-2 mb-3" style="padding-top: 10px;text-align: right; ">{{ disp_targetFaceSizeLength }}</CRow>
+      <CRow>
+        <CCol sm="6">
+          <CInput size="lg"  class="h5"  style="width: 100%;" />
+        </CCol>
+      </CRow>
 
-              <!-- <vxe-table-column field="enable" :title="disp_enable" min-width="12%">
-                  <template #default="{ row }"> 
-                    <vxe-switch v-model="row.enable" v-on:change="activeStatusChange(row)"></vxe-switch>
-                  </template>
-              </vxe-table-column> -->
-              
-            </vxe-table>
-          </div>
-
-          <vxe-pager :layouts="[
-              'PrevJump',
-              'PrevPage',
-              'Number',
-              'NextPage',
-              'NextJump',
-              'FullJump',
-              'Total',
-            ]" 
-            :current-page="value_tablePage.currentPage" 
-            :page-size="value_tablePage.pageSize"
-            :total="value_tablePage.totalResult" 
-            @page-change="handlePageChange">
-          </vxe-pager>
-        </CCardBody>
-      </CCard>
-
+      <!-- Card Access -->
+      <CRow sm="12" class="h2 ml-2 mb-3 mt-5" style="padding-top: 10px;text-align: right; ">{{ disp_cardAccessTitle }}</CRow>
+      <CRow>
+        <CCol sm="6">
+          <CSelect
+            size="lg"
+            v-model="localStep2form.name"
+            placeholder="請選擇"
+            :options="cardAccessOptions"
+          />
+        </CCol>
+      </CRow>
 
     </div>
-  </template>
-    
-  <script>
-    import i18n from "@/i18n";
-    import { mapState } from "vuex";
+  </div>
+</template>
 
-    import VueSelect from 'vue-select';
-    import Multiselect from "vue-multiselect";
-    import "@/airacss/vue-multiselect.css";
+<script>
+import i18n from "@/i18n";
 
-    export default {
-      name: "AddCamerasStep2Form",
-      props:{
-        step2form: Object
+export default {
+  name: "AddTabletsStep2Form",
+  props: {
+    step2form: Object,
+    defaultValues: Object,
+    isFieldPassed: Function,
+  },
+  data() {
+    return {
+      localStep2form: { ...this.step2form },
+
+      /*Face access title  */
+      disp_faceAccessTitle: i18n.formatter.format("TabletsBasicTitleNameFaceAccess"),
+
+      /**content */
+      disp_recognitionThreshold: i18n.formatter.format("TabletsBasicCOlNameRecognitionThreshold"),
+      disp_faceCaptureInternal: i18n.formatter.format("TabletsBasicCOlNameFaceCaptureInternal"),
+      disp_faceOverlapRatio: i18n.formatter.format("TabletsBasicCOlNameFaceOverlapRatio"),
+      disp_targetFaceSizeLength: i18n.formatter.format("TabletsBasicCOlNameTargetFaceSizeLength"),
+
+      /*Card access title  */
+      disp_cardAccessTitle: i18n.formatter.format("TabletsBasicTitleNameCardAccess"),
+
+      // data
+      cardAccessOptions:["A","B"]
+    };
+  },
+  watch: {
+    localStep2form: {
+      handler(newValue) {
+        this.$emit("updateStep2form", { ...newValue });
       },
-      data() {
-      return {
-          localStep2form: { ...this.step2form },
-
-          isChecked: true,
-
-          value_dataItemsToShow: [{enable:false, name:'Cameras #1', group:'Clock-in'}, {enable:false, name:'Cameras #2', group:'Entrance'}],
-          value_allTableItems: [],
-          value_tablePage: {
-            currentPage: 1,
-            pageSize: 5,
-            totalResult: 0,
-          },
-          value_searchingFilter: "",
-
-          /*btn */
-          disp_search: i18n.formatter.format("Search"),
-
-          /*data table */
-          disp_outputDevices: i18n.formatter.format("VideoDeviceCameras"),
-          disp_group: i18n.formatter.format("VideoDevicesGroup"),
-
-          /**v-model */
-          value_deviceGroups: "", /**選單 */
-          value_deviceGroupsList: [1,2,3]
-        };
+      deep: true,
+    },
+    defaultValues: {
+      handler(newValue) {
+        Object.entries(newValue).forEach(([key, value]) => {
+          if (!Object.keys(this.step2form).includes(key)) return;
+          this.localStep2form[key] = value;
+        });
       },
-      components: {
-        "v-select": VueSelect,
-        multiselect: Multiselect,
-      },
-      computed: {
-        ...mapState(["ellipsisMode"]),
-      },
-      //預設值
-      created() {
-      
-      }, 
-      // 拿資料 寫入資料
-      watch: {
-        // localStep2form: {
-        //   handler(newValue) {
-        //     console.log('emit updateStep2form')
-        //     this.$emit('updateStep2form', { ...newValue });
-        //   },
-        //   deep: true,
-        // },
-      },
-      methods: {
-        //分頁處理
-        handlePageChange({ currentPage, pageSize }) {
-          const self = this;
-          self.value_tablePage.currentPage = currentPage;
-          self.value_tablePage.pageSize = pageSize;
-          self.value_dataItemsToShow = self.generateFilteredData(self.value_allTableItems,self.value_searchingFilter);
-          self.resizeOneTable();
-        },
-        refreshTableItems(cb) {
-          const self = this;
-          if(self.onFetchDataCallback) {
-          self.onFetchDataCallback(function (error, reset, more, tableItems) {
-              // console.log("Form",error, reset, more, tableItems)
-              // console.log("FormDT", tableItems)
-              if (!error) {
-                if (reset) {
-                  self.value_allTableItems = [];
-                  self.value_dataItemsToShow = [];
-                }
-                if (tableItems) {
-                  self.value_allTableItems = self.value_allTableItems.concat(tableItems);
-                  self.value_dataItemsToShow = self.generateFilteredData(
-                    self.value_allTableItems,
-                    self.value_searchingFilter
-                  );
-                  // console.log(self.value_allTableItems,"value_allTableItems")
-                  // console.log(self.value_dataItemsToShow,"value_dataItemsToShow")
-                }
-                if (!more && cb) cb();
-              } else if (cb) cb();
-            });
-          } else if (cb) cb();
-        },
-        // 表格資料處理及搜尋
-        generateFilteredData(sourceData, filter) {
-          const self = this;
+      deep: true,
+    },
+  },
+};
+</script>
 
-          //關鍵字搜尋  item.name裡面看有沒有找到filter
-          const filteredItems = filter.length == 0 ? sourceData : sourceData.filter((item) => {
-                  return (
-                    item.name.toLowerCase().indexOf(filter.toLowerCase()) > -1
-                  );
-                });
-
-          self.value_tablePage.totalResult = filteredItems.length; /**總筆數 */
-
-          const sliceList = filteredItems.slice(
-            (self.value_tablePage.currentPage - 1) * self.value_tablePage.pageSize,
-            self.value_tablePage.currentPage * self.value_tablePage.pageSize
-          );
-
-          return Object.assign([], sliceList);
-        },
-        // 判斷欄位空值
-        isNotEmpty(value) {
-          return value !== null && value !== undefined && value !== '';
-        },
-        headerCellStyle(row, column, rowIndex, columnIndex) {
-          return "fontSize: 18px";
-        },
-          cellStyle(row, column, rowIndex, columnIndex) {
-          return "fontSize:18px;";
-        },
-      },
-    }
-  </script>
-    
-
-  <style>
-    @import url('https://unpkg.com/vue-select@latest/dist/vue-select.css');
-  </style>
