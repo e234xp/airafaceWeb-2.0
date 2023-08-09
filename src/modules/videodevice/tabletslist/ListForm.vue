607367dcad9c81e2c3ef08c5d12da5c6365b72e5
boll840113 <stephy.tsai@aira.com.tw>
2023-08-09 11:50:31 +0800

    Tablet搜尋頁完成

--- src/modules/videodevice/tabletslist/ListForm.vue
+++ src/modules/videodevice/tabletslist/ListForm.vue
@@ -11,85 +11,141 @@
     <div>
       <CCol sm="12">
         <CRow>
-            <div>
-              <CButton size="lg" class="btn btn-primary mr-3 mb-3" >
-                {{ disp_add }}
-              </CButton>
-            </div>
-            <div>
-              <CButton
-                class="btn btn-danger mb-3"
-                size="lg"
-                @click="clickOnMultipleDelete()"
-              >
-                {{ disp_delete }}
-              </CButton>
-            </div>
-            <div style="margin-left: auto">
-              <CInput
-                v-model.lazy="value_searchingFilter"
-                style="width: 400px"
-                size="lg"
-                :placeholder="disp_search"
-              >
-                <template #prepend-content>
-                  <CIcon name="cil-search" />
-                </template>
-              </CInput>
-            </div>
+          <div>
+            <CButton
+              size="lg"
+              class="btn btn-primary mr-3 mb-3"
+              @click="handleOnAdd()"
+            >
+              {{ disp_add }}
+            </CButton>
+          </div>
+          <div>
+            <CButton
+              class="btn btn-danger mb-3"
+              size="lg"
+              @click="handleOnMultipleDelete()"
+            >
+              {{ disp_delete }}
+            </CButton>
+          </div>
+          <div style="margin-left: auto">
+            <CInput
+              v-model.lazy="value_searchingFilter"
+              style="width: 400px"
+              size="lg"
+              :placeholder="disp_search"
+            >
+              <template #prepend-content>
+                <CIcon name="cil-search" />
+              </template>
+            </CInput>
+          </div>
         </CRow>
       </CCol>
     </div>
-    <CButton class="btn btn-outline-primary btn-w-normal mb-3" size="lg" 
-      @click="$router.push('TabletsBasic')">
+    <CButton
+      class="btn btn-outline-primary btn-w-normal mb-3"
+      size="lg"
+      @click="$router.push('TabletsBasic')"
+    >
       假按鈕
     </CButton>
-    <CButton class="btn btn-outline-primary btn-w-normal mb-3" size="lg" 
-      @click="$router.push('TabletsAccessSettings')">
+    <CButton
+      class="btn btn-outline-primary btn-w-normal mb-3"
+      size="lg"
+      @click="$router.push('TabletsAccessSettings')"
+    >
       TabletsAccessSettings
     </CButton>
 
     <!-- 下方資料 -->
     <CCard>
       <CCardBody>
-        <!-- {{ value_dataItemsToShow }} -->
         <div>
-          <vxe-table :data="value_dataItemsToShow"  stripe align="center" :cell-style="cellStyle"
-            :header-cell-style="headerCellStyle" ref="mainTable" :auto-resize="true" keep-source
-              highlight-current-row>
-
-            <vxe-table-column type="checkbox" align="center" width="auto"></vxe-table-column>
+          <vxe-table
+            :data="value_dataItemsToShow"
+            stripe
+            align="center"
+            :cell-style="
+              () => {
+                return 'fontSize:18px;';
+              }
+            "
+            :header-cell-style="
+              () => {
+                return 'fontSize:18px;';
+              }
+            "
+            ref="mainTable"
+            :auto-resize="true"
+            keep-source
+            highlight-current-row
+            :edit-config="{ trigger: 'manual', mode: 'row' }"
+          >
+            <vxe-table-column
+              type="checkbox"
+              align="center"
+              width="auto"
+            ></vxe-table-column>
 
             <vxe-table-column field="enable" :title="disp_enable" width="auto">
-              <template #default="{ row }"> 
-                <vxe-switch v-model="row.enable" v-on:change="activeStatusChange(row)"></vxe-switch>
+              <template #default="{ row }">
+                <vxe-switch
+                  v-model="row.enable"
+                  v-on:change="activeStatusChange(row)"
+                ></vxe-switch>
               </template>
             </vxe-table-column>
 
-            <vxe-table-column :show-overflow="ellipsisMode" field="name" :title="disp_deviceName" align="center" width="auto"></vxe-table-column>
+            <vxe-table-column
+              :show-overflow="ellipsisMode"
+              field="name"
+              :title="disp_deviceName"
+              align="center"
+              width="auto"
+            ></vxe-table-column>
 
-            <vxe-table-column :show-overflow="ellipsisMode" field="timestamp" :title="disp_status" width="auto" align="center">
+            <vxe-table-column
+              :show-overflow="ellipsisMode"
+              field="status"
+              :title="disp_status"
+              width="auto"
+              align="center"
+            >
             </vxe-table-column>
 
-            <vxe-table-column :show-overflow="ellipsisMode" field="remark" :title="disp_ipAddress" width="auto" align="center">
-            </vxe-table-column>
-            
-            <vxe-table-column :show-overflow="ellipsisMode" field="modifier" :title="disp_In" width="auto" align="center">
+            <vxe-table-column
+              :show-overflow="ellipsisMode"
+              field="ip_address"
+              :title="disp_ipAddress"
+              width="auto"
+              align="center"
+            >
             </vxe-table-column>
 
-            <vxe-table-column :show-overflow="ellipsisMode" field="remark1" :title="disp_out" width="auto" align="center">
-            </vxe-table-column>
+            <vxe-table-column min-width="8%">
+              <template #default="{ row }">
+                <div class="d-flex flex-column align-items-center">
+                  <vxe-button
+                    class="btn-in-cell-primary btn-in-cell"
+                    @click="handleOnModify(row)"
+                    >{{ disp_modify }}</vxe-button
+                  >
 
-            <!-- <vxe-table-column field="enable" :title="disp_enable" min-width="12%">
-                <template #default="{ row }"> 
-                  <vxe-switch v-model="row.enable" v-on:change="activeStatusChange(row)"></vxe-switch>
-                </template>
-            </vxe-table-column> -->
-            
+                  <vxe-button
+                    class="btn-in-cell-danger btn-in-cell"
+                    @click="handleOnSingleDelete(row)"
+                    >{{ disp_delete }}</vxe-button
+                  >
+                </div>
+              </template>
+            </vxe-table-column>
           </vxe-table>
         </div>
 
-        <vxe-pager :layouts="[
+        <vxe-pager
+          :layouts="[
             'PrevJump',
             'PrevPage',
             'Number',
@@ -97,117 +153,202 @@
             'NextJump',
             'FullJump',
             'Total',
-          ]" 
-          :current-page="value_tablePage.currentPage" 
+          ]"
+          :current-page="value_tablePage.currentPage"
           :page-size="value_tablePage.pageSize"
-          :total="value_tablePage.totalResult" 
-          @page-change="handlePageChange">
+          :total="value_tablePage.totalResult"
+          @page-change="handlePageChange"
+        >
         </vxe-pager>
       </CCardBody>
     </CCard>
-
   </div>
 </template>
-  
+
 <script>
-  import { mapState } from "vuex";
-  import TableObserver from "@/utils/TableObserver.vue";
-  import i18n from "@/i18n";
+import { mapState } from "vuex";
+import TableObserver from "@/utils/TableObserver.vue";
+import i18n from "@/i18n";
 
-  export default {
-    name: 'Tablets',
-    data() {
-      return {
-        value_dataItemsToShow: [{enable:false,name:'',timestamp:'',remark:'',modifier:'',remark1:''}],
-        value_allTableItems: [],
-        value_tablePage: {
-          currentPage: 1,
-          pageSize: 5,
-          totalResult: 0,
+export default {
+  name: "Tablets",
+  mixins: [TableObserver],
+  props: {
+    onAdd: { type: Function },
+    onDelete: { type: Function },
+    onModify: { type: Function },
+    onGetItems: { type: Function },
+  },
+  data() {
+    return {
+      value_dataItemsToShow: [
+        {
+          enable: false,
+          name: "",
+          timestamp: "",
+          remark: "",
+          modifier: "",
+          remark1: "",
         },
-        value_searchingFilter: "",
-
-        disp_header: i18n.formatter.format("VideoDeviceTablets"),
-        disp_search: i18n.formatter.format("Search"),
-        disp_add: i18n.formatter.format("Add"),
-        disp_delete: i18n.formatter.format("Delete"),
-
-
-        /*data table */
-        disp_enable: i18n.formatter.format("Enable"),
-        disp_deviceName: i18n.formatter.format("DeviceName"),
-        disp_status: i18n.formatter.format("DeviceStatus"),
-        disp_ipAddress: i18n.formatter.format("IpAddress"),
-        disp_In: i18n.formatter.format("EnableIn"),
-        disp_out: i18n.formatter.format("EnableOut"),
-
-      };
-    },
-    computed: {
-      ...mapState(["ellipsisMode"]),
-    },
-    methods: {
-      //分頁處理
-      handlePageChange({ currentPage, pageSize }) {
-        const self = this;
-        this.value_tablePage.currentPage = currentPage;
-        this.value_tablePage.pageSize = pageSize;
-        //this.value_dataItemsToShow = this.generateFilteredData(this.value_allTableItems);
-        this.resizeOneTable();
+      ],
+      value_allTableItems: [],
+      value_tablePage: {
+        currentPage: 1,
+        pageSize: 5,
+        totalResult: 0,
       },
-      // 表格資料處理及搜尋
-      generateFilteredData(sourceData, filter) {
-        const self = this;
+      value_searchingFilter: "",
 
-        //關鍵字搜尋  item.name裡面看有沒有找到filter
-        const filteredItems = self.value_keyword.length == 0 ? sourceData : sourceData.filter((item) => {
-                return (
-                  item.name.toLowerCase().indexOf(self.value_keyword.toLowerCase()) > -1
-                );
-              });
+      disp_header: i18n.formatter.format("VideoDeviceCameras"),
+      disp_search: i18n.formatter.format("Search"),
+      disp_add: i18n.formatter.format("Add"),
+      disp_delete: i18n.formatter.format("Delete"),
+      disp_modify: i18n.formatter.format("Modify"),
 
-        self.value_tablePage.totalResult = filteredItems.length; /**總筆數 */
+      /*data table */
+      disp_enable: i18n.formatter.format("Enable"),
+      disp_deviceName: i18n.formatter.format("DeviceName"),
+      disp_status: i18n.formatter.format("DeviceStatus"),
+      disp_ipAddress: i18n.formatter.format("IpAddress"),
+    };
+  },
+  updated() {
+    const self = this;
 
+    self.value_dataItemsToShow.forEach((item) => {
+      const modifyButtonId = "actionOnModify_" + item.uuid;
+      const deleteButtonId = "actionOnDelete_" + item.uuid;
 
-        const sliceList = filteredItems.slice(
-          (self.value_tablePage.currentPage - 1) * self.value_tablePage.pageSize,
-          self.value_tablePage.currentPage * self.value_tablePage.pageSize
+      let new_deleteButton = null;
+      let new_modifyButton = null;
+      let old_deleteButton = document.getElementById(deleteButtonId);
+      let old_modifyButton = document.getElementById(modifyButtonId);
+      if (old_deleteButton && old_deleteButton.parentNode) {
+        new_deleteButton = old_deleteButton.cloneNode(true);
+        old_deleteButton.parentNode.replaceChild(
+          new_deleteButton,
+          old_deleteButton
         );
-        console.log(sliceList,"sliceListABCS")
-        return Object.assign([], sliceList);
-      },
-      clickOnAdd() {
-        console.log("ADD")
-      },
-      clickOnMultipleDelete() {
-        const self = this;
-        const list = this.$refs.mainTable.getCheckboxRecords();
-        if (list.length > 0) {
-          self.$confirm("", i18n.formatter.format("ConfirmToDelete"), "question", {
-            confirmButtonText: i18n.formatter.format("Confirm"),
-            cancelButtonText: i18n.formatter.format("Cancel"),
-            confirmButtonColor: "#20a8d8",
-            cancelButtonColor: "#f86c6b",
-          })
-          .then((v) => {
-            //self.deleteItem(list);
-          })
-          .catch((e) => {
-            if (cb) cb(false);
-          });
-        }
-      },
-      //切換 enable 開關
-      activeStatusChange(item) {
-        console.log("ABC")
-      },
-      headerCellStyle(row, column, rowIndex, columnIndex) {
-        return "fontSize: 18px";
-      },
-        cellStyle(row, column, rowIndex, columnIndex) {
-        return "fontSize:18px;";
-      },
+      }
+
+      if (old_modifyButton && old_modifyButton.parentNode) {
+        new_modifyButton = old_modifyButton.cloneNode(true);
+        old_modifyButton.parentNode.replaceChild(
+          new_modifyButton,
+          old_modifyButton
+        );
+      }
+
+      if (new_deleteButton)
+        new_deleteButton.addEventListener("click", function () {
+          self.handleOnSingleDelete(item);
+        });
+      if (new_modifyButton)
+        new_modifyButton.addEventListener("click", function () {
+          self.handleOnModify(item);
+        });
+    });
+  },
+  computed: {
+    ...mapState(["ellipsisMode"]),
+  },
+  watch: {
+    value_searchingFilter: function () {
+      const self = this;
+      self.value_tablePage.currentPage = 1;
+      this.value_dataItemsToShow = this.generateFilteredData(
+        this.value_allTableItems,
+        this.value_searchingFilter
+      );
     },
-  }
+  },
+  async mounted() {
+    const self = this;
+    self.refreshTableItems();
+  },
+  methods: {
+    //分頁處理
+    handlePageChange({ currentPage, pageSize }) {
+      this.value_tablePage.currentPage = currentPage;
+      this.value_tablePage.pageSize = pageSize;
+      this.value_dataItemsToShow = this.generateFilteredData(
+        this.value_allTableItems,
+        this.value_searchingFilter
+      );
+    },
+    async refreshTableItems() {
+      const self = this;
+      const tableItems = await self.onGetItems();
+
+      self.value_allTableItems = self.value_allTableItems.concat(tableItems);
+      self.value_dataItemsToShow = self.generateFilteredData(
+        self.value_allTableItems,
+        self.value_searchingFilter
+      );
+    },
+    // 表格資料處理及搜尋
+    generateFilteredData(sourceData, filter) {
+      const self = this;
+
+      //關鍵字搜尋  item.name裡面看有沒有找到filter ip_address
+      const filteredItems =
+        filter.length == 0
+          ? sourceData
+          : sourceData.filter((item) => {
+              return (
+                item.name.toLowerCase().indexOf(filter.toLowerCase()) > -1 ||
+                item.ip_address.toLowerCase().indexOf(filter.toLowerCase()) > -1
+              );
+            });
+      self.value_tablePage.totalResult = filteredItems.length; /**總筆數 */
+
+      const sliceList = filteredItems.slice(
+        (self.value_tablePage.currentPage - 1) * self.value_tablePage.pageSize,
+        self.value_tablePage.currentPage * self.value_tablePage.pageSize
+      );
+
+      return Object.assign([], sliceList);
+    },
+    handleOnAdd() {
+      this.onAdd(this.value_allTableItems);
+    },
+    handleOnModify(item) {
+      this.onModify(item);
+    },
+    deleteItem(listToDel) {
+      const self = this;
+
+      self.onDelete(listToDel, function (success) {
+        if (!success) return;
+
+        const uuidsToDel = listToDel.map(({ uuid }) => uuid);
+        self.value_allTableItems = self.value_allTableItems.filter((item) => {
+          return !uuidsToDel.includes(item.uuid);
+        });
+
+        self.value_dataItemsToShow = self.generateFilteredData(
+          self.value_allTableItems,
+          self.value_searchingFilter
+        );
+      });
+    },
+    handleOnSingleDelete(item) {
+      const self = this;
+
+      if (!item) return;
+      self.deleteItem([item]);
+    },
+    handleOnMultipleDelete() {
+      const self = this;
+      const list = this.$refs.mainTable.getCheckboxRecords();
+
+      if (list.length === 0) return;
+
+      self.deleteItem(list);
+    },
+    activeStatusChange() {
+      console.log("ABC");
+    },
+  },
+};
 </script>
-  
