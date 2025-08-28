bccdd7204112855b4f91f2ae1484fd4aa5184c52
Lelia <bb84729@gmail.com>
2025-08-28 12:30:46 +0800

    feat: 🎸 hearder加入點選關於彈窗

--- src/containers/TheHeaderDropdownAccnt.vue
+++ src/containers/TheHeaderDropdownAccnt.vue
@@ -1,54 +1,68 @@
 <template>
-  <CDropdown inNav>
-    <template #toggler>
-      <CHeaderNavLink>
-        <div v-if="flag_login">
-          <i class="fas fa-user"></i>
-          {{ value_username }}
-        </div>
-      </CHeaderNavLink>
-    </template>
-    <CDropdownItem @click="logout()">
-      <CIcon name="cil-lock-locked" />{{ $t("Logout") }}
-    </CDropdownItem>
-  </CDropdown>
+  <div>
+    <CDropdown in-nav>
+      <template #toggler>
+        <CHeaderNavLink>
+          <div v-if="flag_login">
+            <i class="fas fa-user" />
+            {{ value_username }}
+          </div>
+        </CHeaderNavLink>
+      </template>
+      <CDropdownItem @click="logout()">
+        <CIcon name="cil-lock-locked" />{{ $t("Logout") }}
+      </CDropdownItem>
+      <CDropdownItem @click="showAboutModal = true">
+        <CIcon name="cil-info" />{{ $t("About") }}
+      </CDropdownItem>
+    </CDropdown>
+
+    <AboutModal
+      v-if="showAboutModal"
+      @close="showAboutModal = false"
+    />
+  </div>
 </template>
 
 <script>
-  // import i18n from '@/i18n';
+import AboutModal from './AboutModal.vue';
 
-  export default {
-    name: 'TheHeaderDropdownAccnt',
-    data() {
-      return {
-        flag_login: false,
-        value_username: '',
-      };
-    },
-    created() {
+export default {
+  name: 'TheHeaderDropdownAccnt',
+  components: {
+    AboutModal,
+  },
+  data() {
+    return {
+      flag_login: false,
+      value_username: '',
+      showAboutModal: false,
+    };
+  },
+  created() {
+    const self = this;
+    const erverTokenInfo = self.$globalServerTokenInfo();
+    self.value_username = '';
+    self.flag_login = (erverTokenInfo && erverTokenInfo.token.length > 0);
+    if (self.flag_login) self.value_username = erverTokenInfo.username;
+  },
+  watch: {
+    // $route (to, from) {
+    $route() {
       const self = this;
       const erverTokenInfo = self.$globalServerTokenInfo();
       self.value_username = '';
       self.flag_login = (erverTokenInfo && erverTokenInfo.token.length > 0);
       if (self.flag_login) self.value_username = erverTokenInfo.username;
     },
-    watch: {
-      // $route (to, from) {
-      $route() {
-        const self = this;
-        const erverTokenInfo = self.$globalServerTokenInfo();
-        self.value_username = '';
-        self.flag_login = (erverTokenInfo && erverTokenInfo.token.length > 0);
-        if (self.flag_login) self.value_username = erverTokenInfo.username;
-      },
+  },
+  methods: {
+    logout() {
+      const self = this;
+      self.flag_login = false;
+      self.value_username = '';
+      self.$globalLogout();
     },
-    methods: {
-      logout() {
-        const self = this;
-        self.flag_login = false;
-        self.value_username = '';
-        self.$globalLogout();
-      },
-    },
-  };
+  },
+};
 </script>
